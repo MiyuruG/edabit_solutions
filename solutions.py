6262b25d38ae56171d1ba74cd69446997ab82aa9
Miyuru Gunatilake <rohangunatilake@gmail.com>
2023-12-26 14:56:16 +0530

    updated function

--- solutions.py
+++ solutions.py
@@ -1,3 +1,7 @@
 def convert(hours, minutes):
+	#solution for converting hours and minutes to seconds
 	z=(3600*hours)+(60*minutes)
 	return z
+Test.assert_equals(convert(1, 0), 3600)
+Test.assert_equals(convert(1, 3), 3780)
+Test.assert_equals(convert(0, 30), 1800)
